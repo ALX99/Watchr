cdd338a89e96f4b72acecc7e53e6d5a77c2b4a03
Johan Lindkvist <johanlindkvist89@gmail.com>
2019-09-25 17:06:03 +0200

    Prepare for merge

--- app/src/main/java/ipren/watchr/view/MainActivity.java
+++ app/src/main/java/ipren/watchr/view/MainActivity.java
@@ -1,16 +1,13 @@
 package ipren.watchr.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationView;
 
 import ipren.watchr.R;
 
@@ -21,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Set up navigation
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
         NavigationUI.setupWithNavController(bottomNav, navController);
--- app/src/main/java/ipren/watchr/view/WatchedFragment.java
+++ app/src/main/java/ipren/watchr/view/WatchedFragment.java
@@ -2,12 +2,11 @@ package ipren.watchr.view;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 import ipren.watchr.R;
 
