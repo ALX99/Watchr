f4fb6a31e6e09220c1eedea9d2f4fdb1143f927a
ALX99 <46844683+ALX99@users.noreply.github.com>
2019-09-28 19:06:47 +0200

    duplicate files

--- app/src/main/java/ipren/watchr/ViewModels/MainViewModelInterface.java
+++
@@ -1,8 +0,0 @@
-package ipren.watchr.ViewModels;
-
-import androidx.lifecycle.LiveData;
-
-// This Interface enables us to swap MainActivitys ViewModel
-public interface MainViewModelInterface {
-
-}
