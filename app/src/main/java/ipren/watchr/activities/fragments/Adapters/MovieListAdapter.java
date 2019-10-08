8b25063a6d0e9c5441998cc75c0c41be5e689ebf
Johan Lindkvist <johanlindkvist89@gmail.com>
2019-10-08 14:10:57 +0200

    Click to go to movie details in browse

--- app/src/main/java/ipren/watchr/activities/fragments/Adapters/MovieListAdapter.java
+++ app/src/main/java/ipren/watchr/activities/fragments/Adapters/MovieListAdapter.java
@@ -4,9 +4,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.FragmentNavigator;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -14,6 +19,7 @@ import java.util.List;
 
 import ipren.watchr.Helpers.Util;
 import ipren.watchr.R;
+import ipren.watchr.activities.fragments.BrowseFragmentDirections;
 import ipren.watchr.dataHolders.Movie;
 
 /**
@@ -59,11 +65,19 @@ public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.Movi
         TextView overview = holder.itemView.findViewById(R.id.movieOverview);
         TextView rating = holder.itemView.findViewById(R.id.movieRating);
         TextView genres = holder.itemView.findViewById(R.id.movieGenres);
+        ConstraintLayout layout = holder.itemView.findViewById(R.id.movieLayout);
 
         title.setText(movieList.get(position).title);
         overview.setText(movieList.get(position).overview);
         rating.setText("Rating: " + movieList.get(position).voteAverage);
         Util.loadImage(image, "https://image.tmdb.org/t/p//w92" + movieList.get(position).posterPath, Util.getProgressDrawable(image.getContext()));
+        // Tap to go to movie details
+        layout.setOnClickListener(v -> {
+            BrowseFragmentDirections.ActionDetail action = BrowseFragmentDirections.actionDetail();
+            // Pass the movie id
+            action.setMovieId(movieList.get(position).id);
+            Navigation.findNavController(layout).navigate(action);
+        });
     }
 
     /**
